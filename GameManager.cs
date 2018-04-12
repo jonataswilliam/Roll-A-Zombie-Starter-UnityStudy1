e94404643775788b2df55675ea60227e03eb801f
Jonatas William <jonatas_2602@hotmail.com>
2018-04-12 20:08:03 -0300

    Add controle para selecao de zombies

--- GameManager.cs
+++ GameManager.cs
@@ -7,23 +7,24 @@ public class GameManager : MonoBehaviour {
 
     private int selectedZombiePosition;
     public GameObject selectedZombie; // Variável para armazenar o zombie selecinado
-    public List<GameObject> zombies; //Cria uma array para add os zombies
+    //public List<GameObject> zombies; //Cria uma array para add os zombies
+    public GameObject[] zombies;
     public Vector3 selectedSize;
     public Vector3 defaultSize;
 
     // Use this for initialization
     void Start () {
-        SelectZombie(selectedZombie);
+        SelectZombie(0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    if(Input.GetKeyDown("left")) { 
-
+	    if(Input.GetKeyDown("left")) {
+            GetZombieLeft();
         }
 
         if(Input.GetKeyDown("right")) {
-
+            GetZombieRight();
         }
 
         if (Input.GetKeyDown("up")) {
@@ -33,15 +34,24 @@ public class GameManager : MonoBehaviour {
 
     void GetZombieLeft () {
         if( selectedZombiePosition == 0 ) {
-            SelectZombie(zombies[3]);
+            SelectZombie(zombies.Length);
+        } else {            
+            SelectZombie(selectedZombiePosition - 1);
+        }
+    }
+
+    void GetZombieRight () {
+         if (selectedZombiePosition == zombies.Length) {
+            SelectZombie(0);
         } else {
-            GameObject newZombie = zombies[selectedZombiePosition - 1];
+            SelectZombie(selectedZombiePosition + 1);
         }
     }
 
 
-    void SelectZombie (GameObject newZombie) {
-        newZombie.transform.localScale = selectedSize;
+    void SelectZombie (int IndexNewZombie) {
+        zombies[selectedZombiePosition].transform.localScale = defaultSize;
+        zombies[IndexNewZombie].transform.localScale = selectedSize;
     }
 
     
